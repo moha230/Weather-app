6d6aba15302fe44389168001591de9ba22111972
AF7626 <AF7626@student.jamk.fi>
2024-05-04 18:06:18 +0300

    modified code and files

---
+++ js/weatherApi.js
@@ -0,0 +1,9 @@
+const apiKey = "c421623a30b0b9fb4b4085ba2e79f095";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchInput = document.querySelector(".search-input");
+const searchButton = document.querySelector(".search-button");
+
+
+
+
